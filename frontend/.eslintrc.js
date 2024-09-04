bf89254f92b04b74bbe732a66812e944c122da7c
handbob <handbob@icloud.com>
2024-09-04 22:40:50 +0200

    update

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,22 +1,22 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-essential',
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    semi: ['error', 'always'], // Enforce semicolons
-    'max-len': ['error', { code: 120 }], // Increased max line length to 120 characters
-    'vue/multi-word-component-names': 'off', // Disabled multi-word component names rule
-    'no-new': 'off', // Disabled no-new rule
-    'no-multi-spaces': 'off',
-  },
-};
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:vue/vue3-essential',
+//     'airbnb-base',
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 12,
+//     sourceType: 'module',
+//   },
+//   rules: {
+//     semi: ['error', 'always'], // Enforce semicolons
+//     'max-len': ['error', { code: 120 }], // Increased max line length to 120 characters
+//     'vue/multi-word-component-names': 'off', // Disabled multi-word component names rule
+//     'no-new': 'off', // Disabled no-new rule
+//     'no-multi-spaces': 'off',
+//   },
+// };
